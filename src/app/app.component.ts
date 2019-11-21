f9be368cada2cd4238fd07565bbf045903444a23
Tomek Ciećwierz <tomasz.ciecwierz@gmail.com>
2019-11-21 16:47:27 +0100

    199

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import {Component, ViewChild} from '@angular/core';
-import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -8,49 +7,66 @@ import {NgForm} from '@angular/forms';
 })
 export class AppComponent {
 
-  @ViewChild('f', {static: false}) signUpForm: NgForm;
-  defaultQuestion = 'pet';
-  answer = '';
-  genders = ['male', 'female'];
-  user = {
-    username: '',
-    email: '',
-    secretQuestion: '',
-    answer: '',
-    gender: ''
-  };
+  // @ViewChild('f', {static: false}) signUpForm: NgForm;
+  // defaultQuestion = 'pet';
+  // answer = '';
+  // genders = ['male', 'female'];
+  // user = {
+  //   username: '',
+  //   email: '',
+  //   secretQuestion: '',
+  //   answer: '',
+  //   gender: ''
+  // };
+  // submitted = false;
+  //
+  // suggestUserName() {
+  //   const suggestedName = 'Superuser';
+  //   // this.signUpForm.setValue({
+  //   //   userData: {
+  //   //     username: suggestedName,
+  //   //     email: ''
+  //   //   },
+  //   //   secret: 'pet',
+  //   //   questionAnswer: '',
+  //   //   gender: 'male'
+  //   // });
+  //   this.signUpForm.form.patchValue({
+  //     userData: {
+  //       username: suggestedName
+  //     }
+  //   });
+  // }
+  //
+  // // onSubmit(form: NgForm) {
+  // //   console.log(form.value);
+  // // }
+  //
+  // onSubmit() {
+  //   this.submitted = true;
+  //   this.user.username = this.signUpForm.value.userData.username;
+  //   this.user.email = this.signUpForm.value.userData.email;
+  //   this.user.secretQuestion = this.signUpForm.value.secret;
+  //   this.user.answer = this.signUpForm.value.questionAnswer;
+  //   this.user.gender = this.signUpForm.value.gender;
+  //   this.signUpForm.reset();
+  // }
+
+  @ViewChild('form', {static: false}) form;
+  selectedSubscription = 'adv';
   submitted = false;
 
-  suggestUserName() {
-    const suggestedName = 'Superuser';
-    // this.signUpForm.setValue({
-    //   userData: {
-    //     username: suggestedName,
-    //     email: ''
-    //   },
-    //   secret: 'pet',
-    //   questionAnswer: '',
-    //   gender: 'male'
-    // });
-    this.signUpForm.form.patchValue({
-      userData: {
-        username: suggestedName
-      }
-    });
-  }
-
-  // onSubmit(form: NgForm) {
-  //   console.log(form.value);
-  // }
+  inputForm = {
+    email: '',
+    subscription: '',
+    password: ''
+  };
 
   onSubmit() {
     this.submitted = true;
-    this.user.username = this.signUpForm.value.userData.username;
-    this.user.email = this.signUpForm.value.userData.email;
-    this.user.secretQuestion = this.signUpForm.value.secret;
-    this.user.answer = this.signUpForm.value.questionAnswer;
-    this.user.gender = this.signUpForm.value.gender;
-    this.signUpForm.reset();
+    this.inputForm.email = this.form.value.email;
+    this.inputForm.subscription = this.form.value.password;
+    this.inputForm.password = this.form.value.subscriptions;
+    this.form.reset();
   }
-
 }
